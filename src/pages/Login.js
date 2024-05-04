d2c9c1da3040274134776a5137a6a2dc2d945878
Elvanss <duy.elvans12@gmail.com>
2024-05-05 00:16:49 +1000

    Updated Authentication

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,32 +7,41 @@ const Login = () => {
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+const handleSubmit = async (e) => {
+  e.preventDefault();
 
-    const requestBody = {
-      username,
-      password,
-    };
+  
 
-    try {
-      const response = await fetch('http://localhost:8080/api/v1/users/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(requestBody),
-      });
-
-      if (response.ok) {
-        navigate('/profile');
-      } else {
-        console.error('Login failed');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-    }
+  const requestBody = {
+    username,
+    password,
   };
+
+  try {
+    const response = await fetch('http://localhost:8080/api/v1/users/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Basic ' + btoa(username + ":" + password),
+    },
+    body: JSON.stringify(requestBody),
+  });
+
+    if (response.ok) {
+      const data = await response.json();
+      const token = data.token; // Assuming the server returns a 'token' property
+
+      // Store the token in the client-side application's state or local storage
+      localStorage.setItem('authToken', token);
+
+      navigate('/profile');
+    } else {
+      console.error('Login failed');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
 
   return (
     <div
